38773c47110fda91fca1c98dded92a70c7d46088
Jhonata Jansen <codejhonatajansen@gmail.com>
2025-06-09 15:21:49 -0300

    add classes do security

---
+++ src/main/java/com/devjansen/usuario/infrastructure/entity/Usuario.java
@@ -0,0 +1,32 @@
+package com.devjansen.usuario.infrastructure.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+
+@Table(name = "usuario")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "senha")
+    private String senha;
+
+
+}
